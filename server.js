d8a733c08158bc63019ab8357f93847621cd4e99
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-02-11 18:56:00 +0530

    ok1

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const dotenv= require('dotenv')
+const color= require('colors')
 
 const app = express()
 const port = process.env.PORT || 5000 
 dotenv.config({path:'./config/config.env'})
+const bootcamps=require('./routes/bootcamps')
+var morgan = require('morgan')
+var conn = require('./config/db')
+const errorHandler = require('./error/error')
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} listening on port ${port}!`))
+app.use(express.json())
+//Dev logging middle ware
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+conn();
+
+app.use('/api/v1/bootcamps',bootcamps)
+
+app.use(errorHandler)
+const server=app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} listening on port ${port}!`.yellow.bold))
+
+
+// const {logger}=require('./middleware/logger') //Custom logger
+// app.use(logger)  
+
+process.on('unhandledRejection',(err)=>{
+
+    console.log(`Error mssg: ${err.message}`.red)
+    server.close(()=>{process.exit(1)})
+})
