2410af625311a0cf1c0777e3fd4dee06a3e64914
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-05-28 18:29:27 +0530

    dep

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose=  require('mongoose');
 const connecttodb= async ()=>{
     mongoose.set("strictQuery", false);
 
-    const conn= await mongoose.connect(process.env.MONGO_URI)
+    const conn= await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true })
 
 
 console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
